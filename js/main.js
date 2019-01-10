ac7e04a6ecd3d25f54dbf033688f4f502fa3306d
Rakhmat <tavozmrr@gmail.com>
2019-01-10 16:23:27 +0500

    Project__10_01-2019

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "270px";
+    document.getElementById("clockbtn").style.display='none';
+    document.getElementsByClassName('closebtn')[0].style.display='block';
+
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
